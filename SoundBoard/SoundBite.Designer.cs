85a1c2b40dcdfe4bf7055bf335d24e032830e49d
InsanityMoose <docoma@gmail.com>
2015-07-15 18:34:11 +0200

    Revert "Revert "Initial Releasee""
    This reverts commit a03f76a0afd4f50ab5a5e2714625788afa00d384.

---
+++ SoundBoard/SoundBite.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace SoundBoard
+{
+    partial class SoundBite
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelFile = new System.Windows.Forms.Label();
+            this.buttonPlay = new System.Windows.Forms.Button();
+            this.buttonHotKey = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelFile
+            // 
+            this.labelFile.AllowDrop = true;
+            this.labelFile.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelFile.Dock = System.Windows.Forms.DockStyle.Left;
+            this.labelFile.Location = new System.Drawing.Point(0, 0);
+            this.labelFile.Name = "labelFile";
+            this.labelFile.Size = new System.Drawing.Size(167, 50);
+            this.labelFile.TabIndex = 0;
+            this.labelFile.Text = "Drop file here...";
+            this.labelFile.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelFile.DragDrop += new System.Windows.Forms.DragEventHandler(this.labelFile_DragDrop);
+            this.labelFile.DragEnter += new System.Windows.Forms.DragEventHandler(this.labelFile_DragEnter);
+            // 
+            // buttonPlay
+            // 
+            this.buttonPlay.Dock = System.Windows.Forms.DockStyle.Right;
+            this.buttonPlay.Location = new System.Drawing.Point(223, 0);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(61, 50);
+            this.buttonPlay.TabIndex = 1;
+            this.buttonPlay.Text = "Play";
+            this.buttonPlay.UseVisualStyleBackColor = true;
+            this.buttonPlay.Click += new System.EventHandler(this.buttonPlay_Click);
+            // 
+            // buttonHotKey
+            // 
+            this.buttonHotKey.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonHotKey.Location = new System.Drawing.Point(167, 0);
+            this.buttonHotKey.Name = "buttonHotKey";
+            this.buttonHotKey.Size = new System.Drawing.Size(56, 50);
+            this.buttonHotKey.TabIndex = 2;
+            this.buttonHotKey.Text = "Hotkey";
+            this.buttonHotKey.UseVisualStyleBackColor = true;
+            this.buttonHotKey.Click += new System.EventHandler(this.buttonHotKey_Click);
+            // 
+            // SoundBite
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.buttonHotKey);
+            this.Controls.Add(this.buttonPlay);
+            this.Controls.Add(this.labelFile);
+            this.Name = "SoundBite";
+            this.Size = new System.Drawing.Size(284, 50);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelFile;
+        private System.Windows.Forms.Button buttonPlay;
+        private System.Windows.Forms.Button buttonHotKey;
+    }
+}
---
+++ SoundBoard/SoundBoard.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CSCore;
+using CSCore.SoundOut;
+using CSCore.Codecs;
+using System.Threading;
+using CSCore.SoundOut.DirectSound;
+using CSCore.CoreAudioAPI;
+using Utilities;
+using System.Configuration;
+using SoundBoard.Properties;
+
+namespace SoundBoard
+{
+    public partial class SoundBoard : Form
+    {
+        List<SoundBite> soundBites = new List<SoundBite>();
+        MMDevice outputDeviceMM;
+        DirectSoundOut outputDeviceDS;
+        private int numSoundBites = 14;
+        globalKeyboardHook gkh = new globalKeyboardHook();
+        private SoundBite sbSetKey;
+        public delegate void OnPlaybackStopHandler(object sender, EventArgs e);
+        public event OnPlaybackStopHandler OnPlaybackStop;
+        private List<SoundBite> SoundBitePlaying = new List<SoundBite>();
+
+        public SoundBoard()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            createSettings();
+            this.KeyPreview = true;
+            gkh.KeyDown += new KeyEventHandler(gkh_KeyDown);
+            OnPlaybackStop += SoundBoard_OnPlaybackStop;
+
+            if (WasapiOut.IsSupportedOnCurrentPlatform)
+            {
+                foreach (MMDevice dev in EnumerateWasapiDevices())
+                {
+                    listOutputs.Items.Add(dev);
+                    if (dev.DeviceID == Properties.Settings.Default["OutputDevice"] as string)
+                        listOutputs.SelectedItem = dev;
+                }
+                if(listOutputs.SelectedItem == null)
+                    listOutputs.Select(0, 1);
+                //listOutputs.Text = listOutputs.Items[0].ToString();
+            }
+
+            for (int i = 0; i < numSoundBites; i++)
+            {
+                addSoundBite(i);
+            }
+
+            
+        }
+
+        void SoundBoard_OnPlaybackStop(object sender, EventArgs e)
+        {
+            foreach(SoundBite sb in SoundBitePlaying)
+            {
+                sb.Stop();
+            }
+        }
+
+        void gkh_KeyDown(object sender, KeyEventArgs e)
+        {
+            //lstLog.Items.Add("Down\t" + e.KeyCode.ToString());
+            foreach(SoundBite sb in soundBites)
+            {
+                if (sb.hotkey == e.KeyCode)
+                    sb.Play();
+            }
+            e.Handled = true;
+        } 
+
+        private bool DoesSettingExist(string settingName)
+        {
+            try
+            {
+                return Properties.Settings.Default.Properties.Cast<SettingsProperty>().Any(prop => prop.Name == settingName);
+            }
+            catch(NullReferenceException e)
+            {
+                return false;
+            }
+        }
+
+        private void createSettings()
+        {
+            if (!DoesSettingExist("numSoundBites"))
+            {
+                SettingsProperty property = new SettingsProperty(Settings.Default.Properties["baseSetting"]);
+                property.Name = "numSoundBites";
+                property.PropertyType = typeof(int);
+                Settings.Default.Properties.Add(property);
+                Settings.Default["numSoundBites"] = numSoundBites;
+            }
+            if (!DoesSettingExist("OutputDevice"))
+            {
+                SettingsProperty property = new SettingsProperty(Settings.Default.Properties["baseSetting"]);
+                property.Name = "OutputDevice";
+                property.PropertyType = typeof(string);
+                Settings.Default.Properties.Add(property);
+                Settings.Default["OutputDevice"] = "Default";
+            }
+            for(int i = 0; i < numSoundBites; i++)
+            {
+                if(!DoesSettingExist("sb"+i))
+                {
+                    SettingsProperty property = new SettingsProperty(Settings.Default.Properties["baseSetting"]);
+                    property.Name = "sb"+i;
+                    property.PropertyType = typeof(string);
+                    Settings.Default.Properties.Add(property);
+                }
+                if (!DoesSettingExist("sbkey" + i))
+                {
+                    SettingsProperty property = new SettingsProperty(Settings.Default.Properties["baseSetting"]);
+                    property.Name = "sbkey" + i;
+                    property.PropertyType = typeof(string);
+                    Settings.Default.Properties.Add(property);
+                }
+            }
+        }
+
+
+
+        private void addSoundBite(int id)
+        {
+            SoundBite sb = new SoundBite();
+            sb.PlayPressed += sb_PlayPressed;
+            sb.OnSoundFileChanged += sb_OnSoundFileChanged;
+            sb.id = id;
+            if (Properties.Settings.Default["sb" + sb.id] != null)
+            {
+                string file = Properties.Settings.Default["sb" + sb.id].ToString();
+                if (System.IO.File.Exists(file))
+                    sb.fileName = file;
+            }
+            
+            // Hotkeys 
+            sb.OnHotkeySetStart += sb_OnHotkeySetStart;
+            string hkstring = Settings.Default["sbkey" + id] as string;
+            KeysConverter keysConverter = new KeysConverter();
+            if (hkstring != "")
+            {
+                Keys hk = (Keys)keysConverter.ConvertFromString(hkstring);
+                if (hk != Keys.None)
+                {
+                    sb.hotkey = hk;
+                    gkh.HookedKeys.Add(hk);
+                }
+            }
+            soundBites.Add(sb);
+            sb.Parent = flowLayoutPanel1;
+        }
+
+        void sb_OnHotkeySetStart(object sender, EventArgs e)
+        {
+            this.KeyPreview = true;
+            this.sbSetKey = sender as SoundBite;
+            this.KeyDown += SoundBoard_KeyDown;
+        }
+
+        void SoundBoard_KeyDown(object sender, KeyEventArgs e)
+        {
+            this.KeyDown -= SoundBoard_KeyDown;
+            this.sbSetKey.hotkey = e.KeyCode;
+            gkh.HookedKeys.Add(e.KeyCode);
+            Properties.Settings.Default["sbkey" + sbSetKey.id] = e.KeyCode.ToString();
+            Properties.Settings.Default.Save();
+        }
+
+        void sb_OnSoundFileChanged(object sender, EventArgs e)
+        {
+            SoundBite sb = sender as SoundBite;
+            Properties.Settings.Default["sb" + sb.id] = sb.fileName;
+            Properties.Settings.Default.Save();
+        }
+
+        void sb_PlayPressed(object sender, EventArgs e)
+        {
+            SoundBite sb = (SoundBite)sender;
+            Thread playThread = new Thread(new ParameterizedThreadStart(PlaySoundFile));
+            SoundBitePlaying.Add(sb);
+            playThread.Start(sb.fileName);
+        }
+
+        private void PlaySoundFile(object filename)
+        {
+            using (IWaveSource soundSource = CodecFactory.Instance.GetCodec(filename as string))
+            {
+                //SoundOut implementation which plays the sound
+                using (ISoundOut soundOut = GetSoundOut())
+                {                   
+                    //Tell the SoundOut which sound it has to play
+                    soundOut.Initialize(soundSource);
+                    
+                    //Play the sound
+                    soundOut.Play();
+
+                    while (soundOut.PlaybackState == PlaybackState.Playing)
+                    {
+                        Thread.Sleep(500);
+                    }
+
+                    //Stop the playback
+                    soundOut.Stop();
+                    OnPlaybackStop(this, null);
+                }
+            }
+        }
+
+        private ISoundOut GetSoundOut()
+        {
+            if (WasapiOut.IsSupportedOnCurrentPlatform)
+            {
+                WasapiOut wasapiOut = new WasapiOut();
+                wasapiOut.Device = outputDeviceMM;
+                return wasapiOut;
+            }
+            else
+            {
+                DirectSoundOut directSoundOut = new DirectSoundOut();
+                directSoundOut.Device = outputDeviceDS.Device;
+                return directSoundOut;
+            }
+                
+        }
+
+        public IEnumerable<DirectSoundDevice> EnumerateDirectSoundDevices()
+        {
+            return new DirectSoundDeviceEnumerator().Devices;
+        }
+
+        public IEnumerable<MMDevice> EnumerateWasapiDevices()
+        {
+            using (MMDeviceEnumerator enumerator = new MMDeviceEnumerator())
+            {
+                return enumerator.EnumAudioEndpoints(DataFlow.Render, DeviceState.Active);
+            }
+        }
+
+        private void listOutputs_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(WasapiOut.IsSupportedOnCurrentPlatform)
+            {
+                outputDeviceMM = listOutputs.SelectedItem as MMDevice;
+                listOutputs.Text = outputDeviceMM.ToString();
+                Properties.Settings.Default["OutputDevice"] = outputDeviceMM.DeviceID;
+                Properties.Settings.Default.Save();
+            }
+            else
+            {
+                outputDeviceDS = listOutputs.SelectedItem as DirectSoundOut;
+                listOutputs.Text = outputDeviceDS.ToString();
+                Properties.Settings.Default["OutputDevice"] = outputDeviceDS.Device.ToString();
+                Properties.Settings.Default.Save();
+            }
+            
+        }
+    }
+}
